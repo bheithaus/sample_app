12f6af241d2b7086b075a866a30fcdbdee8069cc
Brian Heithaus <bheithaus@gmail.com>
2013-02-08 20:20:18 -0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   
+  
+  before_save :create_remember_token
   before_save { |user| user.email = email.downcase }
   
   validates :password, presence: true, length: { minimum: 6 }
@@ -22,4 +24,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   					uniqueness: { case_sensitive: false }
+  					
+private
+		def create_remember_token
+		  self.remember_token = SecureRandom.urlsafe_base64
+		end
 end
